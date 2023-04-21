ba2b7120c7bd4010c1edbfb213ef78fd7da4d475
russellh76 <russell.hogg@gmail.com>
2023-04-21 01:20:15 -0400

    Ported MartaCard over to use scriptable
    So far it seems like the scriptable browser might be slower than the pythonista browser.

--- MartaCard.js
+++ MartaCard.js
@@ -3,8 +3,18 @@
 
 
 
+
+
+
+
 var card=args.queryParameters.card;
 var name=args.queryParameters.name;
+
+
+name="Russell";
+card="01640324490645579529";
+
+
 var url="https://balance.breezecard.com/breezeWeb/cardnumber_qa.do?submitButton.x=0&submitButton.y=0&cardnumber=";
 url=url+card;
 let wv=new WebView();
@@ -25,16 +35,21 @@ getData=getData+'    }';
 getData=getData+'    var cash = document.querySelectorAll("td")[document.querySelectorAll("td").length-9].innerHTML;';
 getData=getData+'    cash=cash.replace("$","");';
 getData=getData+'    var balanceprotected = document.querySelectorAll("b")[0].innerHTML;';
-getData=getData+'    var hotlisted = document.querySelectorAll("b)[1].innerHTML;';
+getData=getData+'    var hotlisted = document.querySelectorAll("b")[1].innerHTML;';
 getData=getData+'    var expiration = document.querySelectorAll("td")[8].innerHTML.replace(/-/g,"/").replace("  : ","");';
 
 
-getData=getData+'    sentence=sentence+"you have "+ trips +" trips remaining,"';
 
 
 
+getData=getData+'    sentence=sentence+"you have "+ trips +" trips remaining,";';
 getData=getData+'    sentence=sentence+" with a cash value of "+cash+" dollars.";';
 getData=getData+'    sentence=sentence+" "+expiration;';
+
+
+
+
+
 
 
 getData=getData+'    if (balanceprotected.indexOf("Yes")>-1){';
@@ -45,6 +60,10 @@ getData=getData+'        sentence=sentence+" and is NOT balance protected.";';
 getData=getData+'    }';
 
 
+
+
+
+
 getData=getData+'    if (hotlisted.indexOf("Yes")>-1){';
 getData=getData+'        sentence=sentence+" Your card is hot listed.";';
 getData=getData+'    }';
@@ -53,7 +72,16 @@ getData=getData+'        sentence=sentence+" Your card is not hot listed.";';
 getData=getData+'    }';
 
 
+
+
+
+
+
 getData=getData+'    sentence=sentence+" You have "+document.getElementsByClassName("Content_normal_black")[document.getElementsByClassName("Content_normal_black").length-1].innerHTML+".";';
+
+
+
+
 getData=getData+'    return sentence;';
 getData=getData+'}';
 getData=getData+'getCard();';
@@ -62,7 +90,7 @@ getData=getData+'getCard();';
 
 let response=await wv.evaluateJavaScript(getData, false);
 log("output:"+response);
-response = encodeURI("Hi, "+name+" "+response);
+response = encodeURI("Hi "+name+", "+response);
 Safari.open("shortcuts://x-callback-url/run-shortcut?name=SpeakScript&input="+response);
 
 
