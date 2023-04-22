e534e7cd5aa876fb0bb7441b0f60eb3f22f63b95
russellh76 <russell.hogg@gmail.com>
2023-04-22 12:48:10 -0400

    Added station URL code to case
    Added station URL code to case statment.

--- casestation.js
+++ casestation.js
@@ -10,39 +10,39 @@
     switch(station) {
       case "E1":  
         MartaStation=" Georgia State ";
-        StationURL = "";   
+        StationURL = "https://itsmarta.com/Georgia-State.aspx";   
         break;
       case "E2":  
         MartaStation=" King Memorial ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/King-Memorial.aspx";
         break;
       case "E3":  
         MartaStation=" Inman Park / Reynoldstown ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Inman-Park.aspx";
         break;
       case "E4":  
         MartaStation=" Edgewood / Candler Park ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Edgewood-Candler-Park.aspx";
         break;
       case "E5":  
         MartaStation=" East Lake ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/East-Lake.aspx";
         break;
       case "E6":  
         MartaStation=" Decatur ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Decatur.aspx";
         break;
       case "E7":  
         MartaStation=" Avondale ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Avondale.aspx";
         break;
       case "E8":  
         MartaStation=" Kensington ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Kensington.aspx";
         break;
       case "E9":  
         MartaStation=" Indian Creek ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Indian-Creek.aspx";
         break;
       case "W0":  
         MartaStation=" Five Points ";
@@ -70,95 +70,95 @@
         break;
       case "P4":  
         MartaStation=" Bankhead ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Bankhead.aspx";
         break;
       case "S1":  
         MartaStation=" Garnett ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Garnett.aspx";
         break;
       case "S2":  
         MartaStation=" West End ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/West-End.aspx";
         break;
       case "S3":  
         MartaStation=" Oakland City ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Oakland-City.aspx";
         break;
       case "S4":  
         MartaStation=" Lakewood / Fort Mcpherson ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Lakewood.aspx";
         break;
       case "S5":  
         MartaStation=" East Point ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/East-Point.aspx";
         break;
       case "S6":  
         MartaStation=" College Park ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/College-Park.aspx";
         break;
       case "S7":  
         MartaStation=" Airport ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Airport.aspx";
         break;
       case "N1":  
         MartaStation=" Peachtree Center ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Peachtree-Center.aspx";
         break;
       case "N2":  
         MartaStation=" Civic Center ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Civic-Center.aspx";
         break;
       case "N3":  
         MartaStation=" North Avenue";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/North-Ave.aspx";
         break;
       case "N4":  
         MartaStation=" Midtown ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Midtown.aspx";
         break;
       case "N5":  
         MartaStation=" Arts Center ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Arts-Center.aspx";
         break;
       case "N6":  
         MartaStation=" Lindbergh Center ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Lindbergh.aspx";
         break;
       case "N7":  
         MartaStation=" Buckhead ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Buckhead.aspx";
         break;
       case "N8":  
         MartaStation=" Medical Center ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Medical-Center.aspx";
         break;
       case "N9":  
         MartaStation=" Dunwoody ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Dunwoody.aspx";
         break;
       case "N10":  
         MartaStation=" Sandy Springs ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Sandy-Springs.aspx";
         break;
       case "N11":  
         MartaStation=" North Springs ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/North-Springs.aspx";
         break;
       case "NE7":  
         MartaStation=" Lenox ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Lenox.aspx";
         break;
       case "NE8":  
         MartaStation=" Brookhaven / Oglethorpe ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Brookhaven.aspx";
         break;
       case "NE9":  
         MartaStation=" Chamblee ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Chamblee.aspx";
         break;
       case "NE10":  
         MartaStation=" Doraville ";
-        StationURL = "";
+        StationURL = "https://itsmarta.com/Doraville.aspx";
         break;
        case "":  
         MartaStation=" ERROR: No Station Provided ";
