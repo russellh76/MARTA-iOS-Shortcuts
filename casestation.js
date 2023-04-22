64cf45bbf1b5431c3b2cae38a9474228307a91ed
russellh76 <russell.hogg@gmail.com>
2023-04-22 12:38:52 -0400

    Work in progress Case statement for stations

--- casestation.js
+++ casestation.js
@@ -3,128 +3,170 @@
     var station = "";
     //station = "E6";
 
+    var MartaStation="none";
+    var StationURL="";
 
     var MartaStation = "";
     switch(station) {
       case "E1":  
         MartaStation=" Georgia State ";
+        StationURL = "";   
         break;
       case "E2":  
         MartaStation=" King Memorial ";
+        StationURL = "";
         break;
       case "E3":  
         MartaStation=" Inman Park / Reynoldstown ";
+        StationURL = "";
         break;
       case "E4":  
         MartaStation=" Edgewood / Candler Park ";
+        StationURL = "";
         break;
       case "E5":  
         MartaStation=" East Lake ";
+        StationURL = "";
         break;
       case "E6":  
         MartaStation=" Decatur ";
+        StationURL = "";
         break;
       case "E7":  
         MartaStation=" Avondale ";
+        StationURL = "";
         break;
       case "E8":  
         MartaStation=" Kensington ";
+        StationURL = "";
         break;
       case "E9":  
         MartaStation=" Indian Creek ";
+        StationURL = "";
         break;
       case "W0":  
         MartaStation=" Five Points ";
+        StationURL = "https://itsmarta.com/Five-Points.aspx";
         break;
       case "W1":  
         MartaStation=" Dome / G W C C / Philips / C N N ";
+        StationURL = "https://itsmarta.com/Omni.aspx";
         break;
       case "W2":  
         MartaStation=" Vine City ";
+        StationURL = "https://itsmarta.com/Vine-City.aspx";
         break;
       case "W3":  
         MartaStation=" Ashby ";
+        StationURL = "https://itsmarta.com/Ashby.aspx";
         break;
       case "W4":  
         MartaStation=" West Lake ";
+        StationURL = "https://itsmarta.com/West-Lake.aspx";
         break;
       case "W5":  
         MartaStation=" Hamilton E Holmes ";
+        StationURL = "https://itsmarta.com/Hamilton-E-Holmes.aspx";
         break;
       case "P4":  
         MartaStation=" Bankhead ";
+        StationURL = "";
         break;
       case "S1":  
         MartaStation=" Garnett ";
+        StationURL = "";
         break;
       case "S2":  
         MartaStation=" West End ";
+        StationURL = "";
         break;
       case "S3":  
         MartaStation=" Oakland City ";
+        StationURL = "";
         break;
       case "S4":  
         MartaStation=" Lakewood / Fort Mcpherson ";
+        StationURL = "";
         break;
       case "S5":  
         MartaStation=" East Point ";
+        StationURL = "";
         break;
       case "S6":  
         MartaStation=" College Park ";
+        StationURL = "";
         break;
       case "S7":  
         MartaStation=" Airport ";
+        StationURL = "";
         break;
       case "N1":  
         MartaStation=" Peachtree Center ";
+        StationURL = "";
         break;
       case "N2":  
         MartaStation=" Civic Center ";
+        StationURL = "";
         break;
       case "N3":  
         MartaStation=" North Avenue";
+        StationURL = "";
         break;
       case "N4":  
         MartaStation=" Midtown ";
+        StationURL = "";
         break;
       case "N5":  
         MartaStation=" Arts Center ";
+        StationURL = "";
         break;
       case "N6":  
         MartaStation=" Lindbergh Center ";
+        StationURL = "";
         break;
       case "N7":  
         MartaStation=" Buckhead ";
+        StationURL = "";
         break;
       case "N8":  
         MartaStation=" Medical Center ";
+        StationURL = "";
         break;
       case "N9":  
         MartaStation=" Dunwoody ";
+        StationURL = "";
         break;
       case "N10":  
         MartaStation=" Sandy Springs ";
+        StationURL = "";
         break;
       case "N11":  
         MartaStation=" North Springs ";
+        StationURL = "";
         break;
       case "NE7":  
         MartaStation=" Lenox ";
+        StationURL = "";
         break;
       case "NE8":  
         MartaStation=" Brookhaven / Oglethorpe ";
+        StationURL = "";
         break;
       case "NE9":  
         MartaStation=" Chamblee ";
+        StationURL = "";
         break;
       case "NE10":  
         MartaStation=" Doraville ";
+        StationURL = "";
         break;
        case "":  
         MartaStation=" ERROR: No Station Provided ";
+        StationURL = "";
         break;
       default: 
         MartaStation=" ERROR: "+station;
+        StationURL = "";
         break;
     } //*/
     
