a86b08a453eb907fb47143a318a8bdd2b987debd
russellh76 <russell.hogg@gmail.com>
2023-07-06 21:25:37 -0400

    Added abbreviation field
    Added abbreviation field

--- Stations_JSON.js
+++ Stations_JSON.js
@@ -2,6 +2,7 @@
 
 var stations = {
   E1: {
+        StationAbbreviation: "E1", 
         MartaStation: " Georgia State ",
         StationURL: "https://itsmarta.com/Georgia-State.aspx",
         StationAddress: "170 Piedmont Ave, SE, Atlanta, GA 30303", 
@@ -9,6 +10,7 @@ var stations = {
         StationGPS: "33.75018633457871, -84.38618046938421"
   },
   E2: {
+        StationAbbreviation: "E2", 
         MartaStation: " King Memorial ",
         StationURL: "https://itsmarta.com/King-Memorial.aspx",
         StationAddress: "377 Decatur St, SE, Atlanta, GA 30312", 
@@ -16,6 +18,7 @@ var stations = {
         StationGPS: "33.749923784168054, -84.37557557211754"
   },
   E3: {
+        StationAbbreviation: "E3", 
         MartaStation: " Inman Park / Reynoldstown ",
         StationURL: "https://itsmarta.com/Inman-Park.aspx",
         StationAddress: "1055 DeKalb Ave, NE, Atlanta, GA 30307", 
@@ -23,6 +26,7 @@ var stations = {
         StationGPS: "33.75767338612786, -84.35254451436396"
   },
   E4: {
+        StationAbbreviation: "E4", 
         MartaStation: " Edgewood / Candler Park ",
         StationURL: "https://itsmarta.com/Edgewood-Candler-Park.aspx",
         StationAddress: "1475 DeKalb Ave, NE, Atlanta, GA 30307", 
@@ -30,6 +34,7 @@ var stations = {
         StationGPS: "33.76191878812466, -84.33963930099395"
   },
   E5: {
+        StationAbbreviation: "E5", 
         MartaStation: " East Lake ",
         StationURL: "https://itsmarta.com/East-Lake.aspx",
         StationAddress: "2260 College Ave, Atlanta, GA 30307", 
@@ -37,6 +42,7 @@ var stations = {
         StationGPS: "33.765148350874895, -84.31295850153049"
   },
   E6: {
+        StationAbbreviation: "E6", 
         MartaStation: " Decatur ",
         StationURL: "https://itsmarta.com/Decatur.aspx",
         StationAddress: "400 Church St, Decatur, GA 30030", 
@@ -44,6 +50,7 @@ var stations = {
         StationGPS: "33.77462519237718,-84.29615194824973"
   },
   E7: {
+        StationAbbreviation: "E7", 
         MartaStation: " Avondale ",
         StationURL: "https://itsmarta.com/Avondale.aspx",
         StationAddress: "915 E Ponce de Leon Ave, Decatur, GA 30030", 
@@ -51,6 +58,7 @@ var stations = {
         StationGPS: "33.775100373911734, -84.28226687238848"
   },
   E8: {
+        StationAbbreviation: "E8", 
         MartaStation: " Kensington ",
         StationURL: "https://itsmarta.com/Kensington.aspx",
         StationAddress: "3350 Kensington Rd, Decatur, GA 30032", 
@@ -58,6 +66,7 @@ var stations = {
         StationGPS: "33.77266478073744, -84.25148898193302"
   },
   E9: {
+        StationAbbreviation: "E9", 
         MartaStation: " Indian Creek ",
         StationURL: "https://itsmarta.com/Indian-Creek.aspx",
         StationAddress: "3901 Durham Park Rd, Stone Mountain, GA 30083", 
@@ -65,6 +74,7 @@ var stations = {
         StationGPS: "33.76976549057783, -84.22967580099167"
   },
   W0: {
+        StationAbbreviation: "W0", 
         MartaStation: " Five Points ",
         StationURL: "https://itsmarta.com/Five-Points.aspx",
         StationAddress: "30 Alabama St SW, Atlanta, GA 30303", 
@@ -72,6 +82,7 @@ var stations = {
         StationGPS: "33.75392023598456,-84.39162662288796"
   },
   W1: {
+        StationAbbreviation: "W1", 
         MartaStation: " Dome / G W C C / Philips / C N N ",
         StationURL: "https://itsmarta.com/Omni.aspx",
         StationAddress: "", 
@@ -79,6 +90,7 @@ var stations = {
         StationGPS: "33.756520946164684, -84.39677637265507"
   },
   W2: {
+        StationAbbreviation: "W2", 
         MartaStation: " Vine City ",
         StationURL: "https://itsmarta.com/Vine-City.aspx",
         StationAddress: "502 Rhodes St, NW, Atlanta, GA 30314", 
@@ -86,6 +98,7 @@ var stations = {
         StationGPS: "33.7565195661912, -84.40397417238654"
   },
   W3: {
+        StationAbbreviation: "W3", 
         MartaStation: " Ashby ",
         StationURL: "https://itsmarta.com/Ashby.aspx",
         StationAddress: "65 Joseph E Lowery Blvd, Atlanta, GA 30314", 
@@ -93,6 +106,7 @@ var stations = {
         StationGPS: "33.75624413068095, -84.41690619533414"
   },
   W4: {
+        StationAbbreviation: "W4", 
         MartaStation: " West Lake ",
         StationURL: "https://itsmarta.com/West-Lake.aspx",
         StationAddress: "80 Anderson Ave, SW, Atlanta, GA 30314", 
@@ -100,6 +114,7 @@ var stations = {
         StationGPS: "33.75319504495991, -84.44524029802737"
   },
   W5: {
+        StationAbbreviation: "W5", 
         MartaStation: " Hamilton E Holmes ",
         StationURL: "https://itsmarta.com/Hamilton-E-Holmes.aspx",
         StationAddress: "70 Hamilton E Holmes Dr, NW, Atlanta, GA 30311", 
@@ -107,6 +122,7 @@ var stations = {
         StationGPS: "33.75438366530488, -84.47035377238693"
   },
   P4: {
+        StationAbbreviation: "P4", 
         MartaStation: " Bankhead ",
         StationURL: "https://itsmarta.com/Bankhead.aspx",
         StationAddress: "1335 Donald Hollowell Pkwy, Atlanta, GA 30318", 
@@ -114,55 +130,63 @@ var stations = {
         StationGPS: "33.77225494494861, -84.42886489802665"
   },
   S1: {
+        StationAbbreviation: "S1", 
         MartaStation: " Garnett ",
         StationURL: "https://itsmarta.com/Garnett.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "225 Peachtree St, SW, Atlanta, GA 30303", 
+        StationCrossStreets: "Brotherton Street at Peachtree Street", 
         StationGPS: "33.7484910142787, -84.39594632329779"
   },
   S2: {
+        StationAbbreviation: "S2", 
         MartaStation: " West End ",
         StationURL: "https://itsmarta.com/West-End.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "680 Lee St, SW, Atlanta, GA 30310", 
+        StationCrossStreets: "Lee Street at Oglethorpe", 
         StationGPS: "33.736036619930154, -84.41367813817436"
   },
   S3: {
+        StationAbbreviation: "S3", 
         MartaStation: " Oakland City ",
         StationURL: "https://itsmarta.com/Oakland-City.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "1400 Lee St, SW, Atlanta, GA 30310", 
+        StationCrossStreets: "Lee Street at Arden Avenue", 
         StationGPS: "33.71692936002462, -84.42512787331452"
   },
   S4: {
+        StationAbbreviation: "S4", 
         MartaStation: " Lakewood / Ft. McPherson ",
         StationURL: "https://itsmarta.com/Lakewood.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "2020 Lee St, SW, Atlanta, GA 30310", 
+        StationCrossStreets: "Lee Street at Lakewood Avenue", 
         StationGPS: "33.7005387953148, -84.42951615848011"
   },
   S5: {
+        StationAbbreviation: "S5", 
         MartaStation: " East Point ",
         StationURL: "https://itsmarta.com/East-Point.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "2848 East Main St, East Point, GA 30344", 
+        StationCrossStreets: "East Main Street at Washington Road", 
         StationGPS: "33.67755787021818, -84.4405312672711"
   },
   S6: {
+        StationAbbreviation: "S6", 
         MartaStation: " College Park ",
         StationURL: "https://itsmarta.com/College-Park.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "3800 Main St, Atlanta, GA 30337", 
+        StationCrossStreets: "East Main Street at Howard Avenue", 
         StationGPS: "33.65166248476533, -84.44880100072415"
   },
   S7: {
+        StationAbbreviation: "S7", 
         MartaStation: " Airport ",
         StationURL: "https://itsmarta.com/Airport.aspx",
-        StationAddress: "", 
-        StationCrossStreets: "", 
+        StationAddress: "6000 S Terminal Pkwy, Atlanta, GA 30337", 
+        StationCrossStreets: "ATL", 
         StationGPS: "33.64080453587645,-84.44624731278977"
   },
   N1: {
+        StationAbbreviation: "N1", 
         MartaStation: " Peachtree Center ",
         StationURL: "https://itsmarta.com/Peachtree-Center.aspx",
         StationAddress: "216 Peachtree St, NE, Atlanta, GA 30303", 
@@ -170,6 +194,7 @@ var stations = {
         StationGPS: "33.75797266886301, -84.3878131023025"
   },
   N2: {
+        StationAbbreviation: "N2", 
         MartaStation: " Civic Center ",
         StationURL: "https://itsmarta.com/Civic-Center.aspx",
         StationAddress: "435 West Peachtree St, NW, Atlanta, GA 30308", 
@@ -177,6 +202,7 @@ var stations = {
         StationGPS: "33.766569789581915, -84.38761890099283"
   },
   N3: {
+        StationAbbreviation: "N3", 
         MartaStation: " North Avenue ",
         StationURL: "https://itsmarta.com/North-Ave.aspx",
         StationAddress: "713 West Peachtree St, NW, Atlanta, GA 30308", 
@@ -184,6 +210,7 @@ var stations = {
         StationGPS: "33.77168350939747, -84.38702857184813"
   },
   N4: {
+        StationAbbreviation: "N4", 
         MartaStation: " Midtown ",
         StationURL: "https://itsmarta.com/Midtown.aspx",
         StationAddress: "41 Tenth St, NE, Atlanta, GA 30309", 
@@ -191,6 +218,7 @@ var stations = {
         StationGPS: "33.7811144283705, -84.38631161382303"
   },
   N5: {
+        StationAbbreviation: "N5", 
         MartaStation: " Arts Center ",
         StationURL: "https://itsmarta.com/Arts-Center.aspx",
         StationAddress: "1255 West Peachtree St, Atlanta, GA 30309", 
@@ -198,6 +226,7 @@ var stations = {
         StationGPS: "33.78923625608165, -84.38727702887373"
   },
   N6: {
+        StationAbbreviation: "N6", 
         MartaStation: " Lindbergh Center ",
         StationURL: "https://itsmarta.com/Lindbergh.aspx",
         StationAddress: "2424 Piedmont Rd, NE, Atlanta, GA 30324", 
@@ -205,6 +234,7 @@ var stations = {
         StationGPS: "33.823106342847396, -84.36944144668193"
   },
   N7: {
+        StationAbbreviation: "N7", 
         MartaStation: " Buckhead ",
         StationURL: "https://itsmarta.com/Buckhead.aspx",
         StationAddress: "3360 Peachtree Rd, NE, Atlanta, GA 30326", 
@@ -212,6 +242,7 @@ var stations = {
         StationGPS: "33.848799445796175, -84.36836033830365"
   },
   N8: {
+        StationAbbreviation: "N8", 
         MartaStation: " Medical Center ",
         StationURL: "https://itsmarta.com/Medical-Center.aspx",
         StationAddress: "5711 Peachtree-Dunwoody Rd, NE, Atlanta, GA 30342", 
@@ -219,6 +250,7 @@ var stations = {
         StationGPS: "33.91066582663132, -84.3515712015336"
   },
   N9: {
+        StationAbbreviation: "N9", 
         MartaStation: " Dunwoody ",
         StationURL: "https://itsmarta.com/Dunwoody.aspx",
         StationAddress: "1118 Hammond Dr, Atlanta, GA 30328", 
@@ -226,6 +258,7 @@ var stations = {
         StationGPS: "33.921257306833674, -84.34442045253438"
   },
   N10: {
+        StationAbbreviation: "N10", 
         MartaStation: " Sandy Springs ",
         StationURL: "https://itsmarta.com/Sandy-Springs.aspx",
         StationAddress: "1101 Mount Vernon Hwy, Atlanta, GA 30338", 
@@ -233,6 +266,7 @@ var stations = {
         StationGPS: "33.93190113490794, -84.3506936886832"
   },
   N11: {
+        StationAbbreviation: "N11", 
         MartaStation: " North Springs ",
         StationURL: "https://itsmarta.com/North-Springs.aspx",
         StationAddress: "7010 Peachtree Dunwoody Rd, Sandy Springs, GA 30328", 
@@ -240,6 +274,7 @@ var stations = {
         StationGPS: "33.94502750398834, -84.35712289988743"
   },
   NE7: {
+        StationAbbreviation: "NE7", 
         MartaStation: " Lenox ",
         StationURL: "https://itsmarta.com/Lenox.aspx",
         StationAddress: "955 East Paces Ferry Rd, NE, Atlanta, GA 30326", 
@@ -247,6 +282,7 @@ var stations = {
         StationGPS: "33.84505206993194, -84.35862848657872"
   },
   NE8: {
+        StationAbbreviation: "NE8", 
         MartaStation: " Brookhaven / Oglethorpe ",
         StationURL: "https://itsmarta.com/Brookhaven.aspx",
         StationAddress: "4047 Peachtree Road, NE, Atlanta, GA 30319", 
@@ -254,6 +290,7 @@ var stations = {
         StationGPS: "33.859975208994115, -84.33933304351426"
   },
   NE9: {
+        StationAbbreviation: "NE9", 
         MartaStation: " Chamblee ",
         StationURL: "https://itsmarta.com/Chamblee.aspx",
         StationAddress: "5200 New Peachtree Road, Chamblee, GA 30341", 
@@ -261,6 +298,7 @@ var stations = {
         StationGPS: "33.88671281400198, -84.30757243995006"
   },
   NE10: {
+        StationAbbreviation: "NE10", 
         MartaStation: " Doraville ",
         StationURL: "https://itsmarta.com/Doraville.aspx",
         StationAddress: "6000 New Peachtree Rd, Doraville, GA 30340", 
